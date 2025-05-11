fae6d8a4e9572c7d637cd74fa9bdb35bea155dfd
franckmaniche6@gmail.com <franckmaniche6@gmail.com>
2025-05-11 23:48:18 +0100

    Derniere configuration d'OAuth

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import nodemailer from 'nodemailer';
+import { google } from 'googleapis';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+dotenv.config();
 import cors from 'cors';
 import path from 'path';
 import {fileURLToPath} from 'url'; // Necessaire pour obtenir le chemin du fichier actuel
 
+
 const PORT = process.env.PORT || 3000;
+const CLIENT_ID = process.env.CLIENT_ID || '545581657563-8eng1p6msj17a7ec2j1gqgto1ct4opdl.apps.googleusercontent.com';
+const CLIENT_SECRET = process.env.CLIENT_SECRET || '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';
+const REDIRECT_URI = process.env.REDIRECT_URI || 'https://developers.google.com/oauthplayground';
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN || '1//04tll4n_jxygECgYIARAAGAQSNwF-L9IrViqjfkJFpWW9j_g5CronASyX6gOg7QR0XPlld-frL9gnftzy88wIrL6zno7gTD2pQpY';
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 // Configuration pour obtenir __dirname dans les modules ES
 const __filename = fileURLToPath(import.meta.url);
@@ -24,16 +35,36 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+async function createTransporter(){
+    try {
+        const accessToken = await oAuth2Client.getAccessToken();
+        
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            auth: {
+                type: 'OAuth2',
+                // user: process.env.EMAIL_USER,
+                // pass: process.env.EMAIL_PASS,
+                user: 'franckmaniche6@gmail.com',
+                // pass: 'uvtp yzvp nzgt ogle',
+                clientId: CLIENT_ID,
+                clientSecret: CLIENT_SECRET,
+                refreshToken: REFRESH_TOKEN,
+                accessToken: accessToken
+            }
+        });
+        return transporter;
     }
-});
+    catch (error) {
+        console.error('Erreur lors de la création du transporteur:', error);
+        throw error;
+    }
+}
 
-app.post('/send-mail', (req, res) => {
+app.post('/send-mail', async (req, res) => {
     const { name, email, subject, comment } = req.body;
 
     if (!name || !email || !subject || !comment) {
@@ -41,9 +72,10 @@ app.post('/send-mail', (req, res) => {
     }
 
     const mailOptions = {
-        from: 'franckmaniche6@gmail.com',
-        to: 'franckmaniche6@gmail.com',
+        from: 'FRANCK MANICHE 😅 <franckmaniche6@gmail.com>',
+        to: 'skillsformodernity@gmail.com',
         subject: subject,
+        text: 'Juste un message de test',
         html: `
             <h3>Bonjour Maniche Franck,</h3>
             <p>Vous avez reçu un message de ${name}.</p>
@@ -57,14 +89,21 @@ app.post('/send-mail', (req, res) => {
         `
     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error('Erreur lors de l\'envoi de l\'email:', error);
-            return res.status(500).json({ error: 'Erreur lors de l\'envoi de l\'email.' });
-        }
-        console.log('Email envoyé:', info.response);
-        res.status(200).json({ message: 'Email envoyé avec succès !' });
-    });
+    try{
+        const transporter = await createTransporter();
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Erreur lors de l\'envoi de l\'email:', error);
+                return res.status(500).json({ error: 'Erreur lors de l\'envoi de l\'email.' });
+            }
+            console.log('Email envoyé:', info.response);
+            res.status(200).json({ message: 'Email envoyé avec succès !' });
+        });
+    } catch (error) {
+        console.error('Erreur lors de l\'envoi de l\'email:', error);
+        res.status(500).json({ error: 'Erreur lors de l\'envoi de l\'email.' });
+    }
 });
 
 // Capture toutes les pages non trouvées et redirige vers index.html
